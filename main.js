a34921dcee69470a6a8fea90086699fda0941099
alexandru233847 <alexutualexino33@gmail.com>
2024-10-28 17:48:16 +0100

    Run the command

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 console.log ("hello node.js");
 
 let pin = 666;
-
 console.log (pin);
 
-// Retrieve the pin variable
+// Recover the pin variable
+const envPin = process.env.PIN;
+console.log (envPin);
+
+// Recovery from program
+const arg = process.argv[2];
+console.log (arg);
